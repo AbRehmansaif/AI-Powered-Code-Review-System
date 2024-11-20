b1f38d012f3f7b82ea3dd87b12e1c29b4fba0acd
Abdul Rehman <=>
2024-11-21 02:25:03 +0500

    create user admin

--- ai_powered_code_review_system/user/admin.py
+++ ai_powered_code_review_system/user/admin.py
@@ -1,3 +1,37 @@
+# admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    
+    list_display = (
+        'user_name', 'email', 
+        'first_name', 'last_name', 
+        'password_1', 'password_2',
+        'is_active', 'created_at',
+        'updated_at'
+    )
+    
+    search_fields = ('user_name', 'email')
+
+    fieldsets = (
+        (None, {'fields': ('user_name', 'email', 'password')}),
+        (_('Personal Info'), {
+            'fields': ('first_name', 'last_name')
+        }),
+        (_('Permissions'), {
+            'fields': (
+                'is_active'
+            )
+        }),
+        (_('Important dates'), {
+            'fields': ('created_at', 'updated_at')
+        }),
+    )
+    
+
+admin.site.register(User, CustomUserAdmin)
